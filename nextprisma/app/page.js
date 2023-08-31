4eabe28084df915c34319d5ab4dc6e346effbc2e
Ethan Storm <stormethan10@gmail.com>
2023-08-31 14:55:13 -0700

    Update page.js

--- nextprisma/app/page.js
+++ nextprisma/app/page.js
@@ -1,12 +1,12 @@
-import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic'
 const HomePage = dynamic(() => import('./HomePage'), {
   ssr: false,
-});
-import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/route';
+})
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
 
-  return <HomePage session={session} />;
+  return <HomePage session={session} />
 }
