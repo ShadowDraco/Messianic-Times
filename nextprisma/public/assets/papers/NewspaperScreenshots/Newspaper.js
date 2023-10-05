3f29f926f66156dad3705c28990677515d5dd1b8
Ethan Storm <stormethan10@gmail.com>
2023-10-05 13:25:20 -0700

    optimize images moreeeeeeee

--- nextprisma/public/assets/papers/NewspaperScreenshots/Newspaper.js
+++ nextprisma/public/assets/papers/NewspaperScreenshots/Newspaper.js
@@ -1,8 +1,30 @@
-import page1 from './1.png'
-import page2 from './2.png'
-import page3 from './3.png'
-import page4 from './4.png'
-import page5 from './5.png'
-import page6 from './6.png'
+import page1 from './1.webp'
+import page2 from './2.webp'
+import page3 from './3.webp'
+import page4 from './4.webp'
+import page5 from './5.webp'
+import page6 from './6.webp'
+
+import trialPage1 from './1.webp'
+import trialPage2 from './2.webp'
+import trialPage3 from './3.webp'
+import trialPage4 from './4.webp'
+import trialPage5 from './5.webp'
+import trialPage6 from './6.webp'
+
+import cover1 from './1.webp'
+import cover2 from './2.webp'
+import cover3 from './3.webp'
+import cover4 from './4.webp'
+import cover5 from './5.webp'
 
 export const Newspaper1 = [page1, page2, page3, page4, page5, page6]
+export const NewspaperCovers = [cover1, cover2, cover3, cover4, cover5]
+export const TrialPaper = [
+  trialPage1,
+  trialPage2,
+  trialPage3,
+  trialPage4,
+  trialPage5,
+  trialPage6,
+]
